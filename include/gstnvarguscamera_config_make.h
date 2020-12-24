00e26fd7842a57d6397d9ce6da62bebc4ba34d0a
Michael de Gans <47511965+mdegans@users.noreply.github.com>
2020-12-23 17:43:50 -0700

    bump version in make config.h

--- include/gstnvarguscamera_config_make.h
+++ include/gstnvarguscamera_config_make.h
@@ -30,7 +30,7 @@
 #define E0CAA875_7EE8_4F6B_9A1C_99C2CF152440
 
 // If the version is modified, don't forget to change it in the VERSION file
-#define PROJ_VER "1.0.0"
+#define PROJ_VER "1.0.1"
 #define PROJ_DESCRIPTION "nVidia ARGUS Camera Source"
 #define BINARY_PACKAGE "NvARGUSCameraSrc"
 #define PROJ_URL "https://github.com/mdegans/gst-nvarguscamerasrc"
